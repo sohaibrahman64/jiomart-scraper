72c4446e4354d39d8d065c5c3b6531f78a7df3e1
sohaibrahman64 <sohaib.rahman64@gmail.com>
2022-07-12 23:16:45 +0530

    Introducing new category -> Home and Kitchen

--- JioMartScraper/pipelines.py
+++ JioMartScraper/pipelines.py
@@ -9,19 +9,25 @@ from itemadapter import ItemAdapter
 import gspread
 import json
 from gspread import Cell
+from gspread.exceptions import WorksheetNotFound
 
 
 class JiomartscraperPipeline:
 
     def __init__(self):
-        gc = gspread.service_account()
-        sheet = gc.open("JioMart Products")
-        worksheet_list = sheet.worksheets()
-        self.worksheet = sheet.worksheet(worksheet_list[0].title)
+        self.gc = gspread.service_account()
+        self.sheet = self.gc.open("JioMart Products")
+        self.worksheet_list = self.sheet.worksheets()
+        self.worksheet = None
         self.items = dict()
         self.list_items = list()
 
-    def write_to_google_sheet_new(self):
+    def write_to_sheet(self, i, sheet_name):
+        try:
+            self.worksheet = self.sheet.worksheet(sheet_name)
+        except WorksheetNotFound:
+            self.worksheet = self.sheet.add_worksheet(title=sheet_name, rows=1000, cols=4)
+
         cells = []
         for i in range(len(self.list_items)):
             item = self.list_items[i]
@@ -47,5 +53,8 @@ class JiomartscraperPipeline:
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
         self.list_items.append(adapter.asdict())
-        self.write_to_google_sheet_new()
+        if spider.name == 'groceries':
+            self.write_to_sheet(1, 'Groceries')
+        elif spider.name == 'home-kitchen':
+            self.write_to_sheet(2, 'Home And Kitchen')
         return item
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from scrapy import cmdline
 
-cmdline.execute("scrapy crawl smart-watches".split())
+cmdline.execute("scrapy crawl home-kitchen".split())
